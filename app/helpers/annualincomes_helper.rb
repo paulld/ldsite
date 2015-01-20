414fe3f657aaaaec3fd4cf8927cdf5f2886a2387
Paul <paul.lugagnedelpon@gmail.com>
2015-01-21 00:58:55 +0800

    Update user table with all columns. Allow sign in with username or email.

--- app/helpers/annualincomes_helper.rb
+++
@@ -1,2 +0,0 @@
-module AnnualincomesHelper
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,18 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+
+  # def login=(login)
+  #   @login = login
+  # end
+
+  # def login
+  #   @login || self.username || self.email
+  # end
+
   has_attached_file :avatar,
                     :storage => :s3,
                     :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
@@ -13,7 +25,8 @@ class User < ActiveRecord::Base
   validates_attachment_content_type :avatar, 
                                     :content_type => /\Aimage\/.*\Z/
 
-  # validates :username, uniqueness: true
+  validates :username, :presence => true, :uniqueness => { :case_sensitive => false }
+
 
   def s3_credentials
     {
@@ -21,5 +34,18 @@ class User < ActiveRecord::Base
       :access_key_id => ENV['S3_ACCESS_KEY_ID'],
       :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
     }
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
   end
 end
